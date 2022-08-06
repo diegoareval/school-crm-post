7fbbef216834ae83a8a805619312a8f9f92cb63d
Diego <diego2000avelar@gmail.com>
2022-08-06 15:16:18 -0600

    login and session

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   root 'static_pages#landing_page'
   get 'privacy_policy', to: 'static_pages#privacy_policy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
