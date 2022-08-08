92df3e6011c3a993f610779c0187545c9e430d24
Diego <diego2000avelar@gmail.com>
2022-08-07 19:07:40 -0600

    fix: issue to sign out

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,10 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
+// import "@hotwired/turbo-rails"
+import { Turbo } from "@hotwired/turbo-rails"
+Turbo.session.drive = false
 import "controllers"
 import "jquery"
+import "jquery-ui"
 import "jquery_ujs"
 import "popper"
 import "bootstrap"
