d17ca4eaca772f5fb8d732dd37d51a1c1ba4ce35
Diego <diego2000avelar@gmail.com>
2022-08-11 17:39:51 -0600

    add timeoutable

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { confirmations: 'confirmations' }
   resources :users, only: [:index, :show, :delete]
+
+  devise_scope :user do
+    get   "/check_session_timeout"    => "session_timeout#check_session_timeout"
+    get   "/session_timeout"          => "session_timeout#render_timeout"
+  end
+  
   root 'static_pages#landing_page'
   get 'privacy_policy', to: 'static_pages#privacy_policy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
